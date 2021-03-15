6a772f35f02dbe08b59f895a5d4a6dcc2fd75084
spthpk <80557704+spthpk@users.noreply.github.com>
2021-03-15 09:59:36 -0300

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,4 +1,4 @@
-const help = (prefix) => {
+[]const help = (prefix) => {
 	return `
 
 ╔══㋠【 𝐇𝐏𝐊 ♱ 𝐒𝐏𝐓 】㋠══
@@ -12,88 +12,88 @@ const help = (prefix) => {
 ║
 ╠══㋠【 MENU 】㋠══
 ║
-║8 *${prefix}fig*
-║9 *${prefix}toimg*
-║10 *${prefix}meme*
-║11 *${prefix}memeindo*
-║12 *${prefix}gtts*
-║13 *${prefix}lolih [on]*
-║16 *${prefix}leens [na legenda]*
-║17 *${prefix}wait [na legenda]*
-║18 *${prefix}setprefix*
-║20 *${prefix}wame*
+║ *${prefix}fig*
+║ *${prefix}toimg*
+║ *${prefix}meme*
+║ *${prefix}memeindo*
+║ *${prefix}gtts*
+║ *${prefix}lolih [on]*
+║ *${prefix}leens [na legenda]*
+║ *${prefix}wait [na legenda]*
+║ *${prefix}setprefix*
+║ *${prefix}wame*
 ║
 ╠══㋠【 IMAGENS 】㋠══
 ║
-║54 *${prefix}loli* [off]
-║55 *${prefix}loli1*
-║56 *${prefix}hentai*
-║58 *${prefix}porno*
-║62 *${prefix}mia*
-║63 *${prefix}mia1*
-║64 *${prefix}mia2*
-║65 *${prefix}belle*
-║66 *${prefix}belle1*
-║68 *${prefix}belle3*
-║69 *${prefix}akeno*
-║70 *${prefix}meme*   
-║71 *${prefix}lofi*
-║72 *${prefix}malkova*
-║74 *${prefix}nsfwloli1*
-║75 *${prefix}reislin*
+║ *${prefix}loli* [off]
+║ *${prefix}loli1*
+║ *${prefix}hentai*
+║ *${prefix}porno*
+║ *${prefix}mia*
+║ *${prefix}mia1*
+║ *${prefix}mia2*
+║ *${prefix}belle*
+║ *${prefix}belle1*
+║ *${prefix}belle3*
+║ *${prefix}akeno*
+║ *${prefix}meme*   
+║ *${prefix}lofi*
+║ *${prefix}malkova*
+║ *${prefix}nsfwloli1*
+║ *${prefix}reislin*
 ║
 ╠══㋠【 INTELIGÊNCIA IA 】㋠══
 ║
-║76 *${prefix}simih 1 (para ativar)*
-║77 *${prefix}simih 0 (para desativar)*
+║ *${prefix}simih 1 (para ativar)*
+║ *${prefix}simih 0 (para desativar)*
 ║ *${prefix}simi (sua mensagem)*
 ║
 ╠══㋠【 PREMIUM 】㋠══
 ║
 ║☆Para ter acesso vip mande: [prefixo]daftarvip☆
 ║
-║81 *${prefix}dado*
-║82 *${prefix}cekvip*
-║83 *${prefix}premiumlist*
-║84 *${prefix}delete*
-║85 *${prefix}modapk*
-║86 *${prefix}indo10*
-║88 *${prefix}qrcode*
-║89 *${prefix}chentai*
-║90 *${prefix}gcpf*
-║91 *${prefix}gbin*
-║92 *${prefix}pack*
-║93 *${prefix}destrava*
-║94 *${prefix}gpessoa*
+║ *${prefix}dado*
+║ *${prefix}cekvip*
+║ *${prefix}premiumlist*
+║ *${prefix}delete*
+║ *${prefix}modapk*
+║ *${prefix}indo10*
+║ *${prefix}qrcode*
+║ *${prefix}chentai*
+║ *${prefix}gcpf*
+║ *${prefix}gbin*
+║ *${prefix}pack*
+║ *${prefix}destrava*
+║ *${prefix}gpessoa*
 ║
 ╠══㋠【 GRUPO 】㋠══
 ║
-║95 *${prefix}banir*
-║98 *${prefix}add*
-║99 *${prefix}promover*
-║100 *${prefix}setfoto [na legenda]*
-║101 *${prefix}setname [texto]*
-║102 *${prefix}rebaixar*
-║103 *${prefix}admins*
-║104 *${prefix}marcar*
-║107 *${prefix}bemvindo [1/0]*
-║108 *${prefix}grupoinfo*
-║109 *${prefix}bomdia*
-║110 *${prefix}boatarde*
-║111 *${prefix}boanoite*
-║112 *${prefix}setdesc*
-║112 *${prefix}antilink*
-║112 *${prefix}antiracismo*
+║ *${prefix}banir*
+║ *${prefix}add*
+║ *${prefix}promover*
+║ *${prefix}setfoto [na legenda]*
+║ *${prefix}setname [texto]*
+║ *${prefix}rebaixar*
+║ *${prefix}admins*
+║ *${prefix}marcar*
+║ *${prefix}bemvindo [1/0]*
+║ *${prefix}grupoinfo*
+║ *${prefix}bomdia*
+║ *${prefix}boatarde*
+║ *${prefix}boanoite*
+║ *${prefix}setdesc*
+║ *${prefix}antilink*
+║ *${prefix}antiracismo*
 ║
 ╠══㋠【 ESPECIFICO DO BOT 】㋠══
 ║
-║115 *${prefix}clonar [@]*
-║119 *${prefix}gay [@]*
-║120 *${prefix}wame*
-║122 *${prefix}setppbot (marque uma img)*
-║123 *${prefix}pinterest (nome)*
-║124 *${prefix}desligar (so para o dono)*
-║125 *${prefix}timer*
+║ *${prefix}clonar [@]*
+║ *${prefix}gay [@]*
+║ *${prefix}wame*
+║ *${prefix}setppbot (marque uma img)*
+║ *${prefix}pinterest (nome)*
+║ *${prefix}desligar (so para o dono)*
+║ *${prefix}timer*
 ║
 ╠══㋠【 INFO 】㋠══
 ║
